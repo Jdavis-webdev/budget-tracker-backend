3dae4e9400115385fb6e574fb08cd3b5906462fc
Jdavis-webdev <fishrifle1234@gmail.com>
2025-02-26 02:14:14 -0600

    Added dotenv support and updated environment variables

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/transactions", transactionRoutes);
 app.use("/api/plaid", plaidRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
